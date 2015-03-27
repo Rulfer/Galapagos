776b077b46c55f8f28049bba99d2846b76313d60
Rulfer <lol_controll@hotmail.com>
2015-03-27 15:48:51 +0100

    Newsfeed Possibly ferdig Endret hvordan den skriver ut nyhetene. Er og laget nye funksjoner som tar for seg viktige nyheter, samt lagt til en queue array som holder på alle viktige nyheter. Funksjonen som skriver ut nyheter sjekker om det er noe i den array før den skriver ut, og om det er noe vil den sortere arrayen etterpå. Er det ingen viktige nyheter vil den skrive ut en random funny thing.

---
+++ Galapagos/Galapagos/Assets/_scripts/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+	//public float newsTimer;
+	//public float newsTimerLimit = 10;
+
+	// Use this for initialization
+	void Start () {
+		//newsTimer = newsTimerLimit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//newsTimerLimit -= Time.deltaTime;
+
+	}
+}
