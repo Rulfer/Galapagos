6dfada127db683f53bdbff86b14bf1c5e8526e8e
Rulfer <lol_controll@hotmail.com>
2015-03-28 16:39:53 +0100

    Random events ish der Koden for Random Events er laget, men det skjer ikke annet enn at det er en timer som tikker ned og at tekst skrives ut når en event skjer.Vil kunne jobbe mer på random events når det faktisk er noen variabler i spillet å leke med. :P
    I samme slengen ble newsfeed fikset på, så den funker 100% nå.

--- Galapagos/Galapagos/Assets/_scripts/GameMaster.cs
+++ Galapagos/Galapagos/Assets/_scripts/GameMaster.cs
@@ -2,17 +2,12 @@
 using System.Collections;
 
 public class GameMaster : MonoBehaviour {
-	//public float newsTimer;
-	//public float newsTimerLimit = 10;
 
 	// Use this for initialization
 	void Start () {
-		//newsTimer = newsTimerLimit;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//newsTimerLimit -= Time.deltaTime;
-
 	}
 }
---
+++ Galapagos/Galapagos/Assets/_scripts/randomEvents.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomEvents : MonoBehaviour {
+	public float timer;
+	public float timerLimit = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		timer = timerLimit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0.0f) {
+			createEvents();
+		}
+	}
+
+	public void createEvents(){
+		int i = Random.Range (1, 6);
+
+		switch (i) {
+		case 1:
+			newsFeed.disasterNews(5);
+			break;
+		case 2:
+			newsFeed.disasterNews(6);
+			break;
+		case 3:
+			newsFeed.disasterNews(7);
+			break;
+		case 4:
+			newsFeed.disasterNews(8);
+			break;
+		case 5:
+			newsFeed.disasterNews(9);
+			break;
+		}
+
+		timer = Random.Range (20,50);
+		Debug.Log ("Time before new event: " + timer);
+	}
+}
