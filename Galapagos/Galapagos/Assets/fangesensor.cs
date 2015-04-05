9c03d08f0568400568e4577a7b52439b2d8fe416
Br4vO <john.haraldstad@hotmail.com>
2015-04-05 14:06:58 +0200

    Startet på Politi og tyvfiskere

---
+++ Galapagos/Galapagos/Assets/fangesensor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fangesensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.W))
+		    {
+		transform.Translate (Vector3.up/20);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate (Vector3.left/20);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate (Vector3.down/20);
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate (Vector3.right/20);
+		}
+
+	
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "tyvfisker")
+		{
+			other.gameObject.SetActive(false);
+		}
+		Debug.Log ("Tyvfiskere kvelt");
+	}
+
+}
